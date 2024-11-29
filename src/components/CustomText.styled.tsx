a877f27b55303e03cc534b22c503bb2720120219
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-11-29 17:52:07 +0300

    Updates

--- src/components/CustomText.styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const CustomText = styled.p`
-  font-weight: 300;
-  font-size: 18px;
-  line-height: 1.44444;
-  color: #ccc;
-`;
--- src/layout/sections/Projects/ProjectTitle.styled.tsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const ProjectTitle = styled.h3`
-  font-size: 1.75rem;
-`;
